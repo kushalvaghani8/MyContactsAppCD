470149ba52ffdfc6f4f7e99d445148e12712b3c6
kushal <kushal_vaghani@yahoo.com>
2022-04-02 17:31:29 -0400

    Initial commit with basic UI

---
+++ MyContactsAppCD/AddContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddContactViewController.swift
+//  MyContactsAppCD
+//
+//  Created by Kushal Vaghani on 02/04/2022.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
